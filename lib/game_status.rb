557c446a54e57905400a124b30f2bfc1cf67774e
David Forer <david.j.forer@gmail.com>
2018-09-05 14:34:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,8 +27,8 @@ WIN_COMBINATIONS = [
       
       if position_1 == "X" && position_2 == "X" && position_3 == "X"
         return win_combo 
-  elsif if position_1  "O" && position_2  "O" && position_3 == "O"
-      
+      elsif if position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combo
     end  
     
   end
